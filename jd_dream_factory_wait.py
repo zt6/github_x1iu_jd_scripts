2244d26b94d0ca9b850777825d75cc615d134ca0
lala <lala@123.com>
2020-12-18 14:12:09 +0800

    add test

---
+++ jd_dream_factory_wait.py
@@ -0,0 +1,26 @@
+import requests
+import time
+import os
+
+if __name__ == "__main__":
+
+    JD_API_HOST = 'https://m.jingxi.com'
+    function_id = 'userinfo/GetUserInfo'
+    body = 'pin=&sharePin=&shareType=&materialTuanPin=&materialTuanId='
+    cookie = os.environ['JD_COOKIE']
+    headers = {
+      'Cookie': cookie,
+      'Host': 'm.jingxi.com',
+      'Accept': '*/*',
+      'Connection': 'keep-alive',
+      'User-Agent': 'jdpingou;iPhone;3.14.4;14.0;ae75259f6ca8378672006fc41079cd8c90c53be8;network/wifi;model/iPhone10,2;appBuild/100351;ADID/00000000-0000-0000-0000-000000000000;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/62;pap/JA2015_311210;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+      'Accept-Language': 'zh-cn',
+      'Referer': 'https://wqsd.jd.com/pingou/dream_factory/index.html',
+      'Accept-Encoding': 'gzip, deflate, br',
+    }
+    url = f'{JD_API_HOST}/dreamfactory/{function_id}?zone=dream_factory&{body}&sceneval=2&g_login_type=1&_time={int(time.time()*1000)}&_={int(time.time()*1000)}'
+
+    get_ret = requests.get(url, headers=headers).json()
+    
+    print(get_ret['data']['user']['currentLevel'])
+
