46697c8378c603d98e8c70d99e6bea0dd1638535
lala <lala@123.com>
2020-12-26 22:05:42 +0800

    调整

--- accwait.py
+++ accwait.py
@@ -6,11 +6,14 @@ import random
 
 padding = 1 # 提前时间
 interval = 1 # 区间
+minute = 59
 
 if len(sys.argv) >= 2:
     padding = float(sys.argv[1])
-if len(sys.argv) == 3:
+if len(sys.argv) >= 3:
     interval = float(sys.argv[2])
+if len(sys.argv) >= 4:
+    minute = int(sys.argv[3])
 
 start_time = 60 - padding - 0.5*interval
 second_setting = int(start_time)
@@ -18,14 +21,13 @@ ms_setting = int((start_time - second_setting)*10**6)
 
 target_time = datetime.datetime.now()
 
-if target_time.minute > 30:
-    target_time = target_time.replace(minute=59, second=second_setting, microsecond=ms_setting)
+target_time = target_time.replace(minute=minute, second=second_setting, microsecond=ms_setting)
 
-    i=0
-    while datetime.datetime.now() < target_time:
-        i += 1
-        if i % 30 == 0:
-            logging.warn(f'{datetime.datetime.now()}未到{target_time}点')
-        time.sleep(0.03)
+i=0
+while datetime.datetime.now() < target_time:
+    i += 1
+    if i % 30 == 0:
+        logging.warn(f'{datetime.datetime.now()}未到{target_time}点')
+    time.sleep(0.01)
 
-    time.sleep(random.random()*interval)
+time.sleep(random.random()*interval)
