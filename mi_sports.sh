7769bfd46bc6d621ebc6a7ab0d2bff9d0361d170
lala <lala@123.com>
2021-01-07 20:06:03 +0800

    修改时间

--- mi_sports.sh
+++ mi_sports.sh
@@ -1,1 +1,1 @@
-curl -G -d 'user=17380415734' -d 'password=a12345678' -d 'step=20102' https://api.muxui.com/release/parameter_api
+curl -G -d 'user=17380415734' -d 'password=a12345678' -d 'step=20209' https://api.muxui.com/release/parameter_api
